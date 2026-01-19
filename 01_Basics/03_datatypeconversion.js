e5a73938117869fecb6016a253d1e8bcf8cfefeb
anshuman9921 <agastyap1997@gmail.com>
2026-01-19 15:38:50 +0000

    conversion of datatypes

---
+++ 01_Basics/03_datatypeconversion.js
@@ -0,0 +1,19 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueinNumber = Number(score);
+console.log(typeof valueinNumber);
+
+// "33" => can convert to number as 33
+// "33abc" => NaN
+// true => 1 and false => 0
+
+let isloggedin = 1
+
+let bolleanLoggedin = Boolean(isloggedin);
+console.log(bolleanLoggedin)
+
+// 1 => true and 0 => false
+// "" => false
+// "hitesh" => true
