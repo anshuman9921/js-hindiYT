d7f35c452dc7748c792ca4d47eae73a33d3abe89
anshuman9921 <agastyap1997@gmail.com>
2026-01-20 15:46:05 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "Hitesh"
+const repoCount = 50
+console.log(name + repoCount);
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Hitesh-hc')
+console.log(gameName.charAt(3))
+
+const newString = gameName.substring(0, 4)
+console.log(newString)
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString)
+
+const teststring = gameName.split('-')
+console.log(teststring)
