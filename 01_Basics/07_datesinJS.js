c8e70c39a788f2bec01df07a566f8b055041c55c
anshuman9921 <agastyap1997@gmail.com>
2026-01-27 15:36:24 +0000

    date and time

---
+++ 01_Basics/07_datesinJS.js
@@ -0,0 +1,13 @@
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(typeof mydate);
+
+
+let mycreateddate = new Date("2026-01-23");
+// console.log(mycreateddate.toLocaleString());
+
+// let myTimestamp = Date.now()
+// console.log(myTimestamp/1000);
+
+console.log(mydate.getDate());
