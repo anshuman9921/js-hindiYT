9f338fdbf84707f5a6f74638f5fbaf237796f3ac
anshuman9921 <agastyap1997@gmail.com>
2026-01-28 17:44:04 +0000

    functions and scopes more

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -35,4 +35,38 @@ function loginuser(username){
 }
 
 // console.log(loginuser("Hitesh"))
-console.log(loginuser())
+// console.log(loginuser())
+
+// ... -> this is called both rest and spread operator
+
+function calculatecartprice(...num1){
+    return num1
+}
+
+// console.log(calculatecartprice(200, 400, 500));
+
+
+const user = {
+    username: "Hitesh",
+    price: 1999
+}
+
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleobject(user)
+
+// handleobject({
+//     username: "Sam",
+//     price: 388
+// });
+
+
+const mynewarray = [200, 400, 100, 600]
+
+function returnsecondvalue(getarray){
+    return getarray[1]
+}
+
+console.log(returnsecondvalue(mynewarray));
