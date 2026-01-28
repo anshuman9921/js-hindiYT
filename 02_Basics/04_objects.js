be7ac1e04e77de6abf8ebeece823421aba7c9666
anshuman9921 <agastyap1997@gmail.com>
2026-01-28 17:01:58 +0000

    objects and api

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -16,7 +16,7 @@ const regularuser = {
     }
 }
 
-console.log(regularuser.fullname.userfullname.firstname);
+// console.log(regularuser.fullname.userfullname.firstname);
 
 const obj1 = {1: "a", 2: "b"};
 const obj2 = {3: "c", 4: "d"};
@@ -25,4 +25,15 @@ const obj2 = {3: "c", 4: "d"};
 
 const obj3 = {...obj1, ...obj2}
 
-console.log(obj3);
+// console.log(obj3);
+
+
+const course = {
+    coursename: "JSinhindi",
+    price: 999,
+    courseInstructor: "Hitesh"
+}
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
